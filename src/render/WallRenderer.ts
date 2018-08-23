adb845118ab4cf948367edadcb01610a8c4ed360
kamsac <kamsac@users.noreply.github.com>
2018-08-23 22:32:18 +0200

    Improve rendering walls.

--- src/render/WallRenderer.ts
+++ src/render/WallRenderer.ts
@@ -2,13 +2,15 @@ import Wall from '../Wall';
 
 export default class WallRenderer {
     public static render(context: CanvasRenderingContext2D, wall: Wall): void {
-        wall.segments.forEach((segment) => {
-            context.beginPath();
-            context.moveTo(segment.a.x, segment.a.y);
-            context.lineTo(segment.b.x, segment.b.y);
-            context.strokeStyle = '#468';
-            context.lineWidth = 2;
+        context.beginPath();
+        context.strokeStyle = '#0f0804';
+        context.fillStyle = '#0f0804';
+        context.lineWidth = 2;
+        context.moveTo(wall.segments[0].a.x, wall.segments[0].a.y);
+        for(let i = 1; i < wall.segments.length; i++) {
+            context.lineTo(wall.segments[i].a.x, wall.segments[i].a.y);
             context.stroke();
-        });
+        }
+        context.fill();
     }
 }
